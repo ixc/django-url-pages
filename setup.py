3ff57996e9520c1bcce1386f5643e6b5e577bbba
Aram Dulyan <aram00@gmail.com>
2016-06-29 14:18:16 +1000

    Fixed packaging info, including wrong package name.

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from django_url_pages import __version__
 import setuptools
 
 setuptools.setup(
-    name='django-url-templates',
+    name='django-url-pages',
     version=__version__,
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    author='Interaction Consortium',
+    author_email='studio@interaction.net.au',
+    url='https://github.com/ixc/django-url-pages',
+    license='BSD',
 )
